035f2aad2772365f75d01844dac318d380f4b0a2
Christian Rädel <christian.raedel@gmail.com>
2014-09-15 17:16:26 +0200

    Rewrite #4.

--- test/router.spec.js
+++ test/router.spec.js
@@ -1,221 +1,234 @@
 global.AUTOBAHN_DEBUG = true;
 
-var Router      = require('../lib/router')
-    , CLogger   = require('node-clogger')
-    , autobahn  = require('autobahn')
-    , WebSocket = require('ws')
-    , chai      = require('chai')
-    , expect    = chai.expect
-    , spies     = require('chai-spies')
-    , promised  = require('chai-as-promised');
+var nightlife  = require('../lib/router')
+    , autobahn = require('autobahn')
+    , chai     = require('chai')
+    , expect   = chai.expect
+    , promised = require('chai-as-promised')
+    , spies    = require('chai-spies');
 
 chai.use(spies).use(promised);
 
-var logger = new CLogger({name: 'router-tests'});
-
-describe('Router:constructor', function () {
-    var router = null;
-
-    beforeEach(function (done) {
-        router = new Router();
-
-        setTimeout(function() { done(); }, 500);
-    });
-
-    afterEach(function (done) {
-        router.shutdown().then(function () {
-            done();
-        }).catch(done);
-    });
-
-    it('should instantiate', function () {
-        expect(router).to.be.an.instanceof(Router);
+describe('Router#constructor', function () {
+    it('should instantiate', function (done) {
+        var router = nightlife.createRouter();
+        expect(router).to.be.an.instanceof(nightlife.Router);
+        expect(router.roles).to.have.property('broker');
+        expect(router.roles).to.have.property('dealer');
+        router.close().then(done).catch(done);
     });
 });
 
 describe('Router:Session', function () {
-    var router = null;
+    var router = null, connection = null, session = null;
 
-    beforeEach(function (done) {
-        router = new Router();
+    before(function (done) {
+        router = nightlife.createRouter();
 
         setTimeout(function() { done(); }, 500);
     });
 
-    afterEach(function (done) {
-        router.shutdown().then(done).catch(done);
-    });
-
-    it('should establish a websocket connection', function (done) {
-        var ws = new WebSocket('ws://localhost:3000/nightlife');
-        ws.on('open', function () {
-            var message = [1, 'inge', {roles: {caller: {}, callee: {}, publisher: {}, subscriber: {}}}];
-            ws.send(JSON.stringify(message));
-        });
-
-        function onmessage(data) {
-            logger.debug('onmessage data:', data);
-            var message = JSON.parse(data);
-            expect(message[0]).to.be.equal(2);
-            expect(message[1]).to.be.a('number');
-            expect(message[2]).to.be.deep.equal({roles: router.roles});
-            ws.close();
-        }
-        var mSpy = chai.spy(onmessage);
-        ws.on('message', mSpy);
-
-        ws.on('error', function (err) {
-            done(err);
-        });
-
+    after(function (done) {
         setTimeout(function () {
-            expect(mSpy).to.have.been.called.once;
-            done();
-        }, 500);
+            router.close().then(done).catch(done);
+        });
     });
 
-    it('should establish a connection', function (done) {
-        //this.timeout(0);
-        var connection = new autobahn.Connection({url: 'ws://localhost:3000/nightlife', realm: 'inge'});
-        connection.onopen = function (session) {
-            expect(session).to.be.an.instanceof(autobahn.Session);
-            connection.close();
+    it('should establish a new session', function (done) {
+        router.createRealm('com.to.inge.world');
+
+        connection = new autobahn.Connection({
+            realm: 'com.to.inge.world',
+            url: 'ws://localhost:3000/nightlife'
+        });
+
+        connection.onopen = function (s) {
+            expect(s).to.be.an.instanceof(autobahn.Session);
+            expect(s.isOpen).to.be.true;
+            session = s;
+            done();
         };
-        connection.onclose = function (reason) {
-            done(reason === 'closed' ? undefined : new Error(reason));
-        };
+
         connection.open();
+    });
+
+    it('should close a session', function (done) {
+        expect(connection).to.be.an.instanceof(autobahn.Connection);
+
+        connection.onclose = function (reason) {
+            expect(reason).to.be.equal('closed');
+            done();
+        };
+
+        connection.close();
     });
 });
 
-describe('Router:Session:Publish/Subscribe', function () {
-    this.timeout(3000);
-    var router = null, connection = null, session = null;
+describe('Router:Publish/Subscribe', function () {
+    var router = null, connection = null, session = null, subscription = null;
 
-    beforeEach(function (done) {
-        router = new Router();
+    before(function (done) {
+        router = nightlife.createRouter();
 
-        setTimeout(function () {
-            connection = new autobahn.Connection({url: 'ws://localhost:3000/nightlife', realm: 'inge'});
-            connection.onopen = function (_session) {
-                session = _session;
+        setTimeout(function() {
+            connection = new autobahn.Connection({
+                realm: 'com.to.inge.world',
+                url: 'ws://localhost:3000/nightlife'
+            });
+
+            connection.onopen = function (s) {
+                session = s;
                 done();
             };
+
             connection.open();
-        }, 2000);
+        }, 500);
     });
 
-    afterEach(function (done) {
-        connection.onclose = function (reason) {
-            router.shutdown().then(done).catch(done);
-        };
+    after(function (done) {
         connection.close();
-    });
 
-    it('should subscribe to a topic', function (done) {
-        session.subscribe('com.example.inge', function () {})
-        .then(function (subscription) {
-            expect(subscription).to.be.an.instanceof(autobahn.Subscription);
-            done();
-        })
-        .catch(function (reason) {
-            done(new Error(reason));
+        setTimeout(function () {
+            router.close().then(done).catch(done);
         });
     });
 
-    it('should unsubscribe from a topic', function (done) {
-        session.subscribe('com.example.inge', function () {})
-        .then(function (subscription) {
-            expect(session.isOpen).to.be.true;
-            session.unsubscribe(subscription)
-            .then(function (unsubscribed) {
-                expect(unsubscribed).to.be.true;
-                done();
-            });
+    function onevent(args, kwargs, details) {
+        expect(args).to.be.ok;
+        expect(kwargs).to.be.ok;
+        expect(details).to.be.ok;
+    }
+    var spyEvent = chai.spy(onevent);
+
+    it('should subscribe to a topic', function (done) {
+        expect(session.isOpen).to.be.true;
+        session.subscribe('com.example.inge', spyEvent)
+        .then(function (s) {
+            subscription = s;
+            done();
         })
-        .catch(function (reason) {
-            done(new Error(reason));
+        .catch(function (err) {
+            done(new TypeError(err.stack));
         });
     });
 
     it('should publish to a topic', function (done) {
-        function onevent(args, kwargs, details) {
-            expect(args).to.be.deep.equal(['hello inge!']);
-            expect(kwargs).to.have.property('to', 'inge');
-            expect(details).to.be.ok;
-        }
-        var spy = chai.spy(onevent);
-
-        session.subscribe('com.example.inge', spy)
-        .then(function () {
-            session.publish('com.example.inge', ['hello inge!'], {to: 'inge'}, {acknowledge: true})
-            .then(function (published) {
-                expect(published).to.have.property('id');
-            });
+        expect(session.isOpen).to.be.true;
+        session.publish('com.example.inge', ['hello inge!'], {to: 'inge'}, {acknowledge: true})
+        .then(function (published) {
+            expect(published).to.have.property('id');
         })
-        .catch(function (reason) {
-            done(new Error(reason));
+        .catch(function (err) {
+            done(new Error(err.stack));
         });
 
         setTimeout(function () {
-            expect(spy).to.have.been.called.once;
+            expect(spyEvent).to.have.been.called.once;
             done();
-        }, 2000);
+        }, 500);
     });
 
-    /*
-    it('should throw an error, if publish to non-subscribed topic', function (done) {
-        session.publish('com.example.inge', ['hello inge!'], {}, {acknowledge: true})
-        .catch(function (err) {
-            expect(err).to.be.an.instanceof(autobahn.Error);
-            done();
-        });
-    });
-    */
-
-    it('should register a remote procedure', function (done) {
-        session.register('com.example.rest', function () {})
-        .then(function (registration) {
-            expect(registration).to.be.an.instanceof(autobahn.Registration);
+    it('should unsubscribe from a topic', function (done) {
+        expect(session.isOpen).to.be.true;
+        session.unsubscribe(subscription)
+        .then(function () {
             done();
         })
-        .catch(function (reason) {
-            done(new Error(reason));
+        .catch(function (err) {
+            done(new Error(err.stack));
+        });
+    });
+});
+
+describe('Router:Remote Procedures', function () {
+    var router = null, connection = null, session = null, registration = null;
+
+    before(function (done) {
+        router = nightlife.createRouter();
+
+        setTimeout(function() {
+            connection = new autobahn.Connection({
+                realm: 'com.to.inge.world',
+                url: 'ws://localhost:3000/nightlife'
+            });
+
+            connection.onopen = function (s) {
+                session = s;
+                done();
+            };
+
+            connection.open();
+        }, 500);
+    });
+
+    after(function (done) {
+        connection.close();
+
+        setTimeout(function () {
+            router.close().then(done).catch(done);
         });
     });
 
-    it('should unregister a remote procedure', function (done) {
-        session.register('com.example.rest', function () {})
-        .then(function (registration) {
-            expect(session.isOpen).to.be.true;
-            session.unregister(registration).then(done)
-            .catch(function (reason) {
-                done(new Error(reason));
-            });
+    function onCall(args, kwargs, details) {
+        expect(args).to.be.deep.equal(['hello inge!']);
+        expect(kwargs).to.have.property('to');
+        expect(details).to.be.ok;
+
+        if (kwargs.to === 'world') {
+            throw new autobahn.Error('com.example.inge.error', args, kwargs);
+        } else {
+            return 'inge';
+        }
+    }
+    var spyCall = chai.spy(onCall);
+
+    it('should register a remote procedure', function (done) {
+        expect(session.isOpen).to.be.true;
+        session.register('com.example.inge', spyCall)
+        .then(function (r) {
+            expect(r).to.have.property('id');
+            registration = r;
+            done();
+        })
+        .catch(function (err) {
+            console.log(err.stack);
         });
     });
 
     it('should call a remote procedure', function (done) {
-        function echo(args, kwargs, details) {
-            expect(args).to.be.deep.equal(['hello inge!']);
-            expect(kwargs).to.have.property('name', 'inge');
-            logger.debug(details);
-            return 'echo';
-        }
+        expect(session.isOpen).to.be.true;
+        session.call('com.example.inge', ['hello inge!'], {to: 'inge'})
+        .then(function (result) {
+            expect(result).to.be.equal('inge');
+            expect(spyCall).to.have.been.called.once;
+            done();
+        })
+        .catch(function (err) {
+            done(new Error(err));
+        });
+    });
 
-        var spy = chai.spy(echo);
+    it('should return an error, if remote procedure throws', function (done) {
+        expect(session.isOpen).to.be.true;
+        session.call('com.example.inge', ['hello inge!'], {to: 'world'})
+        .catch(function (err) {
+            expect(err).to.be.an.instanceof(autobahn.Error);
+            expect(err.error).to.be.equal('com.example.inge.error');
+            expect(err.args).to.be.deep.equal(['hello inge!']);
+            expect(err.kwargs).to.have.property('to', 'world');
+            expect(spyCall).to.have.been.called.twice;
+            done();
+        });
+    });
 
-        session.register('com.example.echo', spy)
-        .then(function (registration) {
-            session.call('com.example.echo', ['hello inge!'], {name: 'inge'})
-            .then(function (result) {
-                expect(result).to.be.equal('echo');
-                expect(spy).to.have.been.called.once;
-                done();
-            })
-            .catch(function (reason) {
-                done(new Error(reason));
-            });
+    it('should unregister a remote procedure', function (done) {
+        expect(session.isOpen).to.be.true;
+        session.unregister(registration)
+        .then(function () {
+            done();
+        })
+        .catch(function (err) {
+            done(new Error(err.stack));
         });
     });
 });
