b17d37b2388ed3f71909ce85f887a305d8196c4a
Christian Rädel <christian.raedel@gmail.com>
2014-09-22 17:39:02 +0200

    Add publisher identification.

--- test/advanced.spec.js
+++ test/advanced.spec.js
@@ -100,7 +100,7 @@ describe('Advanced Router:Black-/Whitelisting', function () {
         }, 500);
     });
 
-    it('should exclude publisher from event defaulted', function (done) {
+    it('should exclude publisher from event per default', function (done) {
         sessionA.publish('com.example.inge', ['default'], {}, {});
 
         setTimeout(function () {
@@ -109,12 +109,106 @@ describe('Advanced Router:Black-/Whitelisting', function () {
         }, 500);
     });
 
-    it('should include publisher into event explicit', function (done) {
+    it('should include publisher explicit into event', function (done) {
         sessionA.publish('com.example.inge', ['exclude_me'], {}, {exclude_me: false});
 
         setTimeout(function () {
-            expect(spy).to.have.been.called.exactly(3);
+            expect(spy).to.have.been.called.exactly(4);
             done();
         }, 500);
+    });
+});
+
+describe('Advanced Router:Publisher Identification', function () {
+    var router = null, connection = null, session = null;
+
+    function onevent(args, kwargs, details) {
+        expect(args).to.be.ok;
+        expect(kwargs).to.be.ok;
+        expect(details).to.be.ok;
+        expect(details).to.have.property('publisher');
+    }
+
+    var spy = chai.spy(onevent);
+
+    before(function (done) {
+        router = nightlife.createRouter();
+
+        setTimeout(function () {
+            connection = new autobahn.Connection({
+                url: 'ws://localhost:3000/nightlife',
+                realm: 'com.example.inge'
+            });
+
+            connection.onopen = function (s) {
+                session = s;
+
+                session.subscribe('com.example.inge', spy)
+                .then(function () {
+                    done();
+                })
+                .catch(function (err) {
+                    done(new Error(err));
+                })
+                .done();
+            };
+
+            connection.open();
+        }, 500);
+    });
+
+    after(function (done) {
+        router.close().then(done).catch(done);
+    });
+
+    it('should identify publisher', function (done) {
+        session.publish('com.example.inge', [], {}, {disclose_me: true, exclude_me: false});
+
+        setTimeout(function () {
+            expect(spy).to.have.been.called.once;
+            done();
+        }, 800);
+    });
+
+    it('should be able to forbid publisher identifying', function (done) {
+        function onevent(args, kwargs, details) {
+            throw new Error('this shouldn\'t happen! ;)');
+        }
+        var spy = chai.spy(onevent);
+
+        session.subscribe('com.example.anon', spy, {disallow_disclose_me: true})
+        .then(function () {
+            session.publish('com.example.anon', [], {}, {disclose_me: true, exclude_me: false});
+
+            setTimeout(function () {
+                expect(spy).to.not.have.been.called;
+                done();
+            }, 500);
+        })
+        .catch(function (err) {
+            done(new Error(err));
+        })
+        .done();
+    });
+
+    it('should be able to automatically identify a publisher', function (done) {
+        function onevent(args, kwargs, details) {
+            expect(details).to.have.property('publisher');
+        }
+        var spy = chai.spy(onevent);
+
+        session.subscribe('com.example.auto_disclose', spy, {auto_disclose: true})
+        .then(function () {
+            session.publish('com.example.auto_disclose', [], {}, {exclude_me: false});
+
+            setTimeout(function () {
+                expect(spy).to.have.been.called.once;
+                done();
+            }, 500);
+        })
+        .catch(function (err) {
+            done(new Error(err));
+        })
+        .done();
     });
 });
